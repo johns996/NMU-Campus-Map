f68822cf0dd8aa100b7557504c0bb76b1cbc8183
Josh Fridstrom <jfridstr@nmu.edu>
2015-02-13 15:31:27 -0500

    Refactored Layer out. Info windws empty on hover

---
+++ Layer.js
@@ -0,0 +1,104 @@
+var Layer = function (layer, map) {
+	this.mouseoverOptions = { fillOpacity: 0.75, strokeOpacity: 1.0, stokeWidth: 5 };
+	this.mouseoutOptions =  { fillOpacity: 0.16, strokeOpactiy: 1.0, strokeWidth: 10 };
+	this.file = layer.baseUrl;
+	this.centerPointMap = {}; // name => center of polygon
+	this.windowPointMap = {}; // name => info window position point
+	this.polygonMap 	= {}; // name => polygon associated with it
+	this.map = map;
+	var self = this;
+	layer.placemarks.forEach(function(each) {
+		//each.polygon.setMap(null);
+		self.centerPointMap[each.name] = each.polygon.bounds.getCenter();
+		self.windowPointMap[each.polygon.title] = self.findEdge(each.polygon.bounds.getCenter(), each.polygon);
+		self.getPolygonNamed(each.polygon.title).add(each.polygon);
+		each.polygon.setOptions(self.mouseoutOptions);
+		google.maps.event.addListener(each.polygon, 'mouseover', function(event) {
+			this.setOptions(self.mouseoverOptions);
+			this.infoWindow.setOptions(self.infoWindowOptions);
+			this.infoWindow.setPosition(self.windowPointMap[this.title]);
+			this.infoWindow.open(self.map);
+		});
+		google.maps.event.addListener(each.polygon, 'mouseout', function(event) {
+			this.setOptions(self.mouseoutOptions);
+		});
+		google.maps.event.clearListeners(each.polygon, 'click');
+		google.maps.event.addListener(each.polygon, 'click', function(event) {
+			toggleInfo( this.title.replace(/\s+/g, '') );
+			self.moveCenterTo( this.title );
+		});
+	});
+};
+
+Layer.prototype.moveCenterTo = function(name) {
+	this.moveCenter( this.centerPointMap[name] );
+};
+
+Layer.prototype.moveCenter = function(point) {
+	if( point === undefined ) return;
+	console.log(point);
+	this.map.setCenter( point );
+};
+
+Layer.prototype.mouseoverPolygon = function(name) {
+	google.maps.event.trigger( this.getPolygonNamed(name).first(), 'mouseover' );
+};
+
+Layer.prototype.findEdge = function(point, poly) {
+	var isLocationOnEdge = google.maps.geometry.poly.isLocationOnEdge;
+	var containsLocation = google.maps.geometry.poly.containsLocation;
+	var LatLng = google.maps.LatLng;
+	var dist = 1;
+	point = new LatLng( point.lat() + (dist/=2), point.lng() );
+	while( dist >= .0000005 ) {
+		if( isLocationOnEdge( point, poly, .000005 ) ) return new LatLng( point.lat() + .000005, point.lng() );
+		if( containsLocation( point, poly ) )
+			point = new LatLng( point.lat() + (dist/=2), point.lng() );
+		else
+			point = new LatLng( point.lat() - (dist/=2), point.lng() );
+	}
+	return point;
+};
+
+Layer.prototype.doPolygons = function(block) {
+	for( var key in this.polygonMap ) {
+		var obj = this.polygonMap[key];
+		block(obj);
+	}
+}
+
+Layer.prototype.hidePolygons = function () {
+	this.doPolygons(function(each) {
+		each.setMap(null);
+	});
+};
+
+Layer.prototype.showPolygons = function() {
+	this.doPolygons(function(each) {
+		each.setMap(this.map);
+	});
+};
+
+Layer.prototype.getPolygonNamed = function(name) {
+	if( this.polygonMap[name] === undefined )
+		this.polygonMap[name] = new PolygonCollection();
+	return this.polygonMap[name];
+};
+
+var PolygonCollection = function () {
+	this.array = [];
+};
+
+PolygonCollection.prototype.add = function(polygon) {
+	this.array.push(polygon);
+};
+
+PolygonCollection.prototype.setMap = function(map) {
+	this.array.forEach(function(each) {
+		each.setMap(map);
+	});
+};
+
+PolygonCollection.prototype.first = function(map) {
+	return this.array[1];
+};
--- virtualTourMap.js
+++ virtualTourMap.js
@@ -1,44 +1,10 @@
 
 var map;
-function VirtualTour() {
+
+function VirtualTour () {
 	var defaultCenter = new google.maps.LatLng(46.5595, -87.4037);
-	var mouseoverOptions = {fillOpacity: 0.75, strokeOpacity: 1.0, strokeWidth: 5};
-	var mouseoutOptions = {fillOpacity: 0.16, strokeOpacity: 1.0, strokeWidth: 10};
-	var buildingCenterPointMap = {}; // building name => center of the polygon
-	var buildingWindowPointMap = {}; // building name => info window position point
-	var buildingPolygonMap = {};     // building name => polygon associated with it
-
-	/* findEdge will find point directly above the given point that lies just above the polygon */
-	function findEdge(point, poly) {
-		var isLocationOnEdge = google.maps.geometry.poly.isLocationOnEdge;
-		var containsLocation = google.maps.geometry.poly.containsLocation;
-		var dist = 1;
-		point = new google.maps.LatLng(point.lat()+(dist/=2), point.lng());
-		while(dist>=.0000005) {
-			if(isLocationOnEdge(point, poly, .000005)) return new google.maps.LatLng(point.lat()+.00005, point.lng());
-			if(containsLocation(point, poly))
-				point = new google.maps.LatLng(point.lat()+(dist/=2), point.lng());
-			else
-				point = new google.maps.LatLng(point.lat()-(dist/=2), point.lng());
-		}
-		return point;
-	}
-
-	// moves the center of the map to the building name
-	function moveCenterTo(name) {
-		moveCenter(buildingCenterPointMap[name]);
-	}
-
-	// moves the center of the map to the given point
-	function moveCenter(point) {
-		if(point === undefined) return;
-		map.setCenter(point);
-	}
-
-	// fires mouseover event on the polygon for given building name
-	function mouseoverPolygon(name) {
-		google.maps.event.trigger(buildingPolygonMap[name], 'mouseover');
-	}
+	var buildingLayer;
+	var parkingLayer;
 
 	function initializeMap() {
 		var mapOptions = {
@@ -48,52 +14,27 @@ function VirtualTour() {
 		};
 		map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 		var myParser = new geoXML3.parser({ map: map, afterParse: myAfterParse, singleInfoWindow: true, zoom: false });
-		document.getElementById("buildings-button").onclick= function parseBuildings() {
-			for( var key in buildingPolygonMap ) {
-				var obj = buildingPolygonMap[key];
-				obj.setMap(map);
-			}
+		document.getElementById("buildings-button").onclick= function () {
+			parkingLayer.hidePolygons();
+			buildingLayer.showPolygons();
 		}
-		document.getElementById("parking-button").onclick= function parseParking() {
-			
+		document.getElementById("parking-button").onclick= function () {
+			buildingLayer.hidePolygons();
+			parkingLayer.showPolygons();
 		}
-		myParser.parse(["Buildings.kml"]);
+		myParser.parse(["Buildings.kml", "Parking.kml"]);
 		function myAfterParse(doc) {
 			console.log("Parsing Completed Successfully");
-			var myLayer = doc.shift();
-			myLayer.placemarks.forEach(function(each) {
-				each.polygon.setMap(null);
-				buildingCenterPointMap[each.name] = each.polygon.bounds.getCenter();
-				buildingWindowPointMap[each.polygon.title] = findEdge(each.polygon.bounds.getCenter(), each.polygon);
-				buildingPolygonMap[each.polygon.title] = each.polygon;
-				each.polygon.setOptions(mouseoutOptions);
-				google.maps.event.addListener(each.polygon, "mouseover", function(event) {
-					//console.log('mouseover');
-					this.setOptions(mouseoverOptions);
-					this.infoWindow.setOptions(this.infoWindowOptions);
-					this.infoWindow.setPosition(buildingWindowPointMap[this.title]);
-					this.infoWindow.open(map);
-				});
-				google.maps.event.addListener(each.polygon, "mouseout", function(event) {
-					//console.log('mouseout');
-					this.setOptions(mouseoutOptions);
-					//this.infoWindow.close();
-				});
-				google.maps.event.clearListeners(each.polygon, "click");
-				google.maps.event.addListener(each.polygon, "click", function(event) {
-					//console.log('click');
-					toggleInfo(this.title.replace(/\s+/g, ''));
-					moveCenterTo(this.title);
-				});
-			});
-			
-			var arg = window.location.search.substring(1);
+			buildingLayer = new Layer(doc.shift(), map);
+			parkingLayer = new Layer(doc.shift(), map);
+			// process args
+			/* var arg = window.location.search.substring(1);
 			if(arg !== undefined && arg != '') {
 				toggleInfo(arg.replace(/-+/g, ''));
 				moveCenterTo(arg.replace(/-+/g, ' '));
 				mouseoverPolygon(arg.replace(/-+/g, ' '));
 				console.log(arg);
-			}
+			}*/
 		}
 	}
 
